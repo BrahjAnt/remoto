d965c386fdfdbfcc49746d870097a3e621ad9041
Mundo56 <125435531+Mundo56@users.noreply.github.com>
2023-05-17 19:17:46 +0200

    version 1.1

--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,25 @@ public class Main {
 		
 		
 	System.out.println("Datos de estudiantes:");
-	System.out.println("Inserte nombre:");
-	String name = sText.nextLine();
-	System.out.println("Inserte apellidos:");
-	String lastName = sText.nextLine();
-	System.out.println("Inserte fecha de nacimiento:");
-	String birthDate = sText.nextLine();
-	System.out.println("DNI:");
-	String dni = sText.nextLine();
-	System.out.println("Estudio previo:");
-	String studies = sText.nextLine();
-	System.out.println("Teléfono:");
-	String numberTelephone = sText.nextLine();
-	
-	System.out.println(name+ lastName+birthDate+ dni+ studies+ numberTelephone);
+	generateDateStudents();
 		
 	}
-
+	public static void generateDateStudents() {
+		Scanner sText = new Scanner(System.in);
+		Scanner sInteger = new Scanner(System.in);
+		System.out.println("Inserte nombre:");
+		String name = sText.nextLine();
+		System.out.println("Inserte apellidos:");
+		String lastName = sText.nextLine();
+		System.out.println("Inserte fecha de nacimiento:");
+		String birthDate = sText.nextLine();
+		System.out.println("DNI:");
+		String dni = sText.nextLine();
+		System.out.println("Estudio previo:");
+		String studies = sText.nextLine();
+		System.out.println("Teléfono:");
+		String numberTelephone = sText.nextLine();
+		
+		System.out.println(name+" " + lastName+" " +birthDate+ " " +dni+ " " +studies+" " + numberTelephone);
+	}
 }
