b1f68ac27b9de741f938523c215917689856dcb6
Mundo56 <125435531+Mundo56@users.noreply.github.com>
2023-05-17 18:36:42 +0200

    version 1.0

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sText = new Scanner(System.in);
+		Scanner sInteger = new Scanner(System.in);
+		
+		
+	System.out.println("Datos de estudiantes:");
+	System.out.println("Inserte nombre:");
+	String name = sText.nextLine();
+	System.out.println("Inserte apellidos:");
+	String lastName = sText.nextLine();
+	System.out.println("Inserte fecha de nacimiento:");
+	String birthDate = sText.nextLine();
+	System.out.println("DNI:");
+	String dni = sText.nextLine();
+	System.out.println("Estudio previo:");
+	String studies = sText.nextLine();
+	System.out.println("Teléfono:");
+	String numberTelephone = sText.nextLine();
+	
+	System.out.println(name+ lastName+birthDate+ dni+ studies+ numberTelephone);
+		
+	}
+
+}
